1ee337fe033faeb15afbfd849c3b6e3a0fcb3394
SavedowW <velikanov-em@yandex.ru>
2021-09-28 16:37:49 +0300

    Added abstract menu element

--- src/AbstractMenu.h
+++ src/AbstractMenu.h
@@ -1,9 +1,23 @@
 #ifndef ABSTRACT_MENU_H_
 #define ABSTRACT_MENU_H_
+#include <vector>
+
+class AbstractMenuElement
+{
+public:
+
+private:
+
+};
 
 class AbstractMenu
 {
-    
+public:
+
+
+private:
+std::vector<AbstractMenuElement* > elems;    
+
 };
 
 #endif
