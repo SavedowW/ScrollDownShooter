b4a6b918567c6f00b3463988694ad203d7a829ab
DeadM00n <velikanov-em@yandex.ru>
2022-08-31 14:44:48 +0300

    Fixed bug with char in TTF_RenderText_Blended

--- src/Core/Symbol.cpp
+++ src/Core/Symbol.cpp
@@ -3,7 +3,8 @@
 
 Symbol::Symbol(Core* core, char ch, TTF_Font* font, const SDL_Color& col)
 {
-	m_tex = core->createText(&ch, col, font);
+	const char buf[2] = { ch, '\0' };
+	m_tex = core->createText(buf, col, font);
 	SDL_QueryTexture(m_tex, NULL, NULL, &m_w, &m_h);
 	if (m_w == 0)
 		std::cout << "Failed creating symbol [" << (int)ch << "]\n";
